d599f94d6c6659ceae47da1fc379ea716a7b6bb5
kaushal powar <knp1532001@gmail.com>
2025-02-27 21:35:08 +0530

    Add embedding generation using local model

--- py_backend/save_embeddings.py
+++ py_backend/save_embeddings.py
@@ -1,14 +1,18 @@
 from llama_index.core import Settings, SimpleDirectoryReader, VectorStoreIndex, get_response_synthesizer
 from llama_index.embeddings.openai import OpenAIEmbedding
-import os
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-embed_model = OpenAIEmbedding(model="text-embedding-3-small", dimensions=512, api_key=OPENAI_API_KEY)
+
+#embed_model = OpenAIEmbedding(model="text-embedding-3-small", dimensions=512, api_key=OPENAI_API_KEY)
+
+embed_model = HuggingFaceEmbedding(
+            model_name="sentence-transformers/all-MiniLM-L6-v2", 
+        )
 Settings.embed_model = embed_model
 
 Settings.chunk_size = 512
 Settings.chunk_overlap = 64
-reader = SimpleDirectoryReader(input_dir= "path-to-docs")
+reader = SimpleDirectoryReader(input_dir= "/Users/kaushal/Documents/Travel-AI/backend/static/docs")
 documents = reader.load_data(show_progress=True, )
 print(f"Chunking completed with {len(documents)} chunks")
 index = VectorStoreIndex.from_documents(
@@ -17,5 +21,6 @@ index = VectorStoreIndex.from_documents(
 
 )
 
-index.storage_context.persist(persist_dir="embeddings3/")
+index.storage_context.persist(persist_dir="embeddings6/")
 print("Embeddings saved")
+
